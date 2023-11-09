4278c77b498a0c3801109280c834572ae748d655
Katie Riches <katiegee2011@gmail.com>
2023-11-10 12:22:13 +1300

    Home page route working

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,18 +1,12 @@
 import { useState, useEffect } from 'react'
+import { Router } from 'express'
+import { Outlet } from 'react-router-dom'
 
-// import  Trivia from './Trivia.tsx'
-// import Animals from './animals.tsx'
-import GeneralKnowledge  from './generalKnowledge.tsx'
+// import Trivia from './Trivia.tsx'
+// // import Animals from './animals.tsx'
+// // import GeneralKnowledge  from './generalKnowledge.tsx'
+// import Home from './Home.tsx'
 
- export default function App() {
-  return (
-    <div>
-     
-      {/* <Trivia /> */}
-      {/* <Animals /> */}
-      <GeneralKnowledge />
-    </div>
-  )
+export default function App() {
+  return <Outlet />
 }
-
-
--- server/routes/welcome.ts
+++ server/routes/welcome.ts
@@ -1,19 +1,9 @@
 import express from 'express'
-import { Welcome } from '../../models/Trivia.ts'
+import Home from '../../client/components/Home.tsx'
 
 const router = express.Router()
 
 // GET /api/v1/welcome/
-router.get('/', (req, res) => {
-  try {
-    res.json({ statement: 'Welcome to external APIs!' } as Welcome)
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).send((err as Error).message)
-    } else {
-      res.status(500).send('Something went wrong')
-    }
-  }
-})
+router.get('/home', (req, res) => {})
 
 export default router
