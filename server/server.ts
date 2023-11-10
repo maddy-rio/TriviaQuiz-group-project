41fd74f027b15ac6fd3b9157db3c0e702fe9c657
Katie Riches <katiegee2011@gmail.com>
2023-11-10 15:07:26 +1300

    Set up dokku

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,20 @@
-import { join } from 'node:path'
 import express from 'express'
 import * as Path from 'node:path'
-import * as URL from 'node:url'
 
 import welcome from './routes/welcome.ts'
-
-const __filename = URL.fileURLToPath(import.meta.url)
-const __dirname = Path.dirname(__filename)
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static(join(__dirname, './public')))
 
 server.use('/api/v1/welcome', welcome)
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
 
 export default server
